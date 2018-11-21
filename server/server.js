64b2558caefa0c714030e7a5f10671914b221b06
Abbas Nazar <abbasnazar.970@gmail.com>
2018-11-22 00:00:38 +0500

    RestApiStart

--- server/server.js
+++ server/server.js
@@ -1,34 +1,26 @@
-var mongoose = require('mongoose');
-mongoose.Promise=global.Promise;
-mongoose.connect("mongodb://localhost:27017/ToDo");
+var express = require('Express');
+var bodyParser = require('body-parser');
 
-var Todo = mongoose.model('Todo',{
-    text:{
-        type:String,
-        required : true,
-        minlength : 1,
-        trim : true
-    },
-    completed:{
-        type: Boolean,
-        default : false
-    },
-    CompletedAt:{
-        type : Number,
-        default : null
+var {mongoose} = require('./db/mongoose');
+var {Todo}= require('./models/todo');
+var {User}=require('./models/user');
 
-    }
+var app= express();
 
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+    var todo = new Todo({
+        text : req.body.text
+    });
+    todo.save().then((doc)=>{
+            res.send(doc);
+    },(err)=>{
+        res.status(400).send(err);
+    })
 });
 
-var newToDo = new Todo({
-    text : '   eat it  ',
-    completed : true,
-    CompletedAt : 2018
-});
 
-newToDo.save().then((doc)=>{
-    console.log('Inserted',doc)
-},(err)=>{
-    console.log("Unable to save",err)
+app.listen(3000,()=>{
+    console.log('Server started on 3000')
 })
