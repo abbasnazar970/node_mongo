ca6790718f9fdd2a7e4216967f3fdffa7459e614
Abbas Nazar <abbasnazar.970@gmail.com>
2018-11-25 17:59:24 +0500

    individualTodo

--- server/server.js
+++ server/server.js
@@ -20,6 +20,21 @@ app.post('/todos',(req,res)=>{
     })
 });
 
+app.get('/todos',(req,res)=>{
+    Todo.find().then((doc)=>{
+            res.send({todos: doc});
+    },(err)=>{
+        res.status(400).send(err);
+    })
+});
+
+app.get('/todos/:id',(req,res)=>{
+    Todo.findById(req.params.id).then((doc)=>{
+            res.send({todos: doc});
+    },(err)=>{
+        res.status(400).send(err);
+    })
+});
 
 app.listen(3000,()=>{
     console.log('Server started on 3000')
